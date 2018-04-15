e39569584da1451d0b0b84613d41fce41e3fa621
Malenczuk <malen11pl@gmail.com>
2018-04-15 21:32:23 +0200

    init EUnit test

---
+++ src/pollution_test.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author Marcin Maleńczuk
+%%% @copyright (C) 2018
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(pollution_test).
+-author("Marcin Maleńczuk").
+
+-include_lib("eunit/include/eunit.hrl").
+-include("pollution_rec.hrl").
+-compile(export_all).
+
+simple_test() ->
+  ?assert(true).
